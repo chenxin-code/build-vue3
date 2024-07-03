01b25de83e798f1e24216013d7d939d202323482
chenxin <540275648@qq.com>
2024-07-03 17:29:15 +0800

    Avue-Crud组件demo

--- src/page/demo/Avue-Crud.vue
+++ src/page/demo/Avue-Crud.vue
@@ -5,7 +5,8 @@
       @row-save="rowSave"
       @row-update="rowUpdate"
       @row-del="rowDel"
-      @refresh-change="refreshChange">
+      @refresh-change="refreshChange"
+      style="margin-top: 60px">
   </avue-crud>
 </template>
 
@@ -16,6 +17,7 @@ import {ElMessage, ElMessageBox} from 'element-plus';
 const data = ref([{name: '张三', age: 22}, {name: '李四', age: 26}]);
 
 const option = ref({
+  rowKey: 'name',
   addBtn: false,
   addRowBtn: true,
   cellBtn: true,
@@ -23,39 +25,31 @@ const option = ref({
 });
 
 const rowSave = (row, done, loading) => {
-  ElMessage.success('新增数据' + JSON.stringify(row));
+  console.log('rowSave', row, done, loading);
   done(row);
+  ElMessage.success('rowSave成功');
 };
 
 const rowUpdate = (row, index, done, loading) => {
-  ElMessage.success('模拟网络请求');
-  setTimeout(() => {
-    ElMessage.success('关闭按钮等待');
-    loading();
-  }, 1000);
-  setTimeout(() => {
-    ElMessage.success('编辑数据' + JSON.stringify(row) + '数据序号' + index);
-    done(row);
-  }, 2000);
+  console.log('rowUpdate', row, index, done, loading);
+  done(row);
+  ElMessage.success('rowUpdate成功');
 };
 
 const rowDel = (row, index, done) => {
-  ElMessageBox.confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+  console.log('rowDel', row, index, done);
+  ElMessageBox.confirm('确定删除吗?', '提示', {
     confirmButtonText: '确定',
     cancelButtonText: '取消',
     type: 'warning'
   }).then(() => {
     done(row);
-    ElMessage({
-      type: 'success',
-      message: '删除成功!'
-    });
-  }).catch(() => {
-  });
+    ElMessage.success('rowDel成功');
+  }).catch(() => {});
 };
 
 const refreshChange = () => {
-  ElMessage.success('刷新回调');
+  ElMessage('刷新');
 };
 </script>
 
